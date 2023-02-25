0f5110fe1be0f138487c4e0b6f1630e6ca7129ce
lokeshwaranv2000 <126314854+lokeshwaranv2000@users.noreply.github.com>
2023-02-24 23:38:37 -0800

    Add files via upload
    Week1.Day1 PROGRAM NO :2(ASSINGMENT)

---
+++ day2/SecondProgram.java
@@ -0,0 +1,28 @@
+package week1.day2;
+
+public class SecondProgram {
+		String mobileBrandName="oppo A57";
+		char mobileLogo='@';
+		short noOfMobilePiece=54;
+		long mobilelmeiNumber=465476587687l;
+		float mobilePrice=18000;
+		boolean isDamaged=false;
+		public static void main(String []args) {
+			SecondProgram loveMobile=new SecondProgram();
+			System.out.println("Name of the Mobile:"+loveMobile.mobileBrandName);
+			System.out.println("Symbol of MobileLogo:"+loveMobile.mobileLogo);
+			System.out.println("Number of Mobilepiece:"+loveMobile.noOfMobilePiece);
+			System.out.println("Mobile Number:"+loveMobile.mobilelmeiNumber);
+			System.out.println("Number of mobilePrice:"+loveMobile.mobilePrice);
+			System.out.println("There ocurred any damage in Mobile:"+loveMobile.isDamaged);
+			
+			
+			
+		}
+		
+		
+		
+			
+		
+
+}
