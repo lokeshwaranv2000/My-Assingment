1a56dc8be1f44e01528fb5581459c666315b8b49
lokeshwaranv2000 <126314854+lokeshwaranv2000@users.noreply.github.com>
2023-02-24 23:22:08 -0800

    Add files via upload
    day1

---
+++ day1/FirstProgram.java
@@ -0,0 +1,46 @@
+package week1.day1;
+
+public class FirstProgram {
+
+		int noOfWheels=2;
+		short noOfMirror=2;
+		long chassisNumber=23447611000237l;
+		boolean IsPunctured=false;
+		String bikeName="Pulsar";
+		double runningKm=120;
+	
+		public static void main(String[] args) {
+			FirstProgram myBike = new FirstProgram();
+		
+		System.out.println("Number of Wheels:"+myBike.noOfWheels);
+		System.out.println("Number of Mirror:"+myBike.noOfMirror);
+		System.out.println("Chassis number:"+myBike.chassisNumber);
+		System.out.println("IsPunctured:"+myBike.IsPunctured);
+		System.out.println("bikeName:"+myBike.bikeName);
+		System.out.println("runningKm:"+myBike.runningKm);
+		
+		
+		
+		
+		
+		
+
+	  
+	
+		
+		
+		
+			
+		
+		
+
+		
+		
+				
+				
+				
+				
+	
+
+}
+}
