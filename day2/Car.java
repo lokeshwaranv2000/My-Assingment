bd9f5a0aabeeb0b93b93d04a1c970ed4d4bba18d
lokeshwaranv2000 <126314854+lokeshwaranv2000@users.noreply.github.com>
2023-02-27 09:40:35 -0800

    Add files via upload
    Assignment 2 week2day1

---
+++ day2/Car.java
@@ -0,0 +1,42 @@
+package week2.day2;
+
+public class Car {
+	public void doorOpen() {
+		System.out.println("Door Open");
+	}
+	public void insertKey() {
+		System.out.println("Insert Key");
+		
+	}
+	public void startCar() {
+		System.out.println("StartCar");
+	}
+	public void switchOnAc() {
+		System.out.println("Switch On Ac");
+		
+	}
+	public void applyBreak() {
+		System.out.println("applyBreak");
+		
+	}
+	public void applyAcclerator() {
+		System.out.println("applyAcclerate");
+	}
+	public static void main(String[] args) {
+		Car Bmw = new Car();
+		Bmw.doorOpen();
+		Bmw.insertKey();
+		Bmw.startCar();
+		Bmw.switchOnAc();
+		Bmw.applyBreak();
+		Bmw.applyAcclerator();
+		
+		
+		
+				
+	}
+	    
+	
+	
+
+}
---
+++ day2/Mymobile.java
@@ -0,0 +1,26 @@
+package week2.day2;
+
+public class Mymobile {
+    public void sendMessage() {
+    	System.out.println("send the Message to the person ");
+    	
+    	
+    }
+    public void shareDocument() {
+    	System.out.println("Share the Document");
+    	
+    }
+    public void call() {
+    	System.out.println("Incoming call or Outgoing call");
+    }
+    public static void main(String[] args) {
+         Mymobile OppoA57 =new Mymobile();
+         OppoA57.sendMessage();
+         OppoA57.shareDocument();
+         OppoA57.call();
+    
+    
+    
+    
+}
+}
